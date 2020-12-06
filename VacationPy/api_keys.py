b22c267d74b704a567b851eae217ac33294009da
Julie Headlee <julieheadlee@gmail.com>
2020-12-05 21:05:13 -0600

    Finished Part 1

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "add here"
+weather_api_key = "6d1b89f189922381406f0908e56db5b3"
 
 # Google API Key
-g_key = "add here"
+g_key = "AIzaSyBakYMwSTl48Jpr6yVSHcb0lwi6QS6Oyq8"
